b8b6d32bf0326eaa9c21b605d1752832f86d59a0
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-09 19:10:47 +0700

    09. Change language globally and manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from "./shared/shared.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+import localeEsHN from '@angular/common/locales/es-HN';
+import localeFrCA from '@angular/common/locales/fr-CA';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsHN);
+registerLocaleData(localeFrCA);
 
 @NgModule({
   declarations: [
@@ -16,7 +23,9 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     AppRoutingModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es-HN'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
