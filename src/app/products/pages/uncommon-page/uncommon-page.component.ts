a75f89b775c291ba236f040b0cbdc2b94648f46a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-09 21:39:36 +0700

    13. i18nPlural Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -10,14 +10,26 @@ export class UncommonPageComponent {
   //i18n Select
   public name: string = 'Muhamad Iffat';
   public gender: 'male' | 'female' = 'male';
+  public clients: string[] = ['Goku', 'Freeza', 'Gohan', 'Picollo', 'Buu'];
 
   public invitationMap = {
     'male': 'Pria',
     'female': 'Wanita'
   };
 
+  public clientsMap = {
+    '=0': 'we don\'t have any clients waiting',
+    '=1': 'we have a client waiting',
+    '=2': 'we have 2 waiting',
+    'other': 'we have # clients waiting'
+  }
+
   changePerson(): void {
     this.name = 'Mellisa';
     this.gender = 'female';
+  }
+
+  deleteClient(): void {
+    this.clients.shift();
   }
 }
